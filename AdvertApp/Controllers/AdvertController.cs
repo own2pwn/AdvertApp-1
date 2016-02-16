a17f76a9e576f726eca31d92dfaead41a7a100cc
Fedorenko <fedorenkoav1993@gmail.com>
2016-02-16 09:05:42 +0200

    work with UI

--- AdvertApp/Controllers/AdvertController.cs
+++ AdvertApp/Controllers/AdvertController.cs
@@ -8,80 +8,8 @@ namespace AdvertApp.Controllers
         // GET: Advert
         public ActionResult Index()
         {
-            var users = new UserRepository();
+            
             return View();
-        }
-
-        // GET: Advert/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Advert/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Advert/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Advert/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Advert/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Advert/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Advert/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
---
+++ AdvertApp/Controllers/LogoController.cs
@@ -0,0 +1,22 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvertApp.Controllers
+{
+    public class LogoController : Controller
+    {
+        // GET: Logo
+        public ActionResult Index()
+        {
+            var repsitory = new UserRepository();
+            repsitory.Save(new Models.DataBaseModels.User { Email = "qe@qwe.qe", Login = "qwe", Name = "qe@qwe.qe", Password = "qwe", Phone = "qwe" });
+
+
+            return View();
+        }
+    }
+}
